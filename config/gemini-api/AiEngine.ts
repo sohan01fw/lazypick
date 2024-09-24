4d66933b7c057df3091c0a261407005cf324ee19
sohan01fw <sohancwactfw@gmail.com>
2024-09-24 13:24:53 +0545

    hide the system instruction

--- config/gemini-api/AiEngine.ts
+++ config/gemini-api/AiEngine.ts
@@ -12,8 +12,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
-  systemInstruction:
-    "focus on generating concise, user-friendly, and relevant questions tailored to the product search process. The tone should be conversational yet professional, guiding the user through essential decision-making points like platform preference (e.g., Amazon, eBay), price range, product specifications, and personal preferences. Ensure clarity and simplicity in the questions, with options that are straightforward, easy to select, and actionable. Prioritize must-ask questions such as platform, budget, and product features, maintaining a helpful and engaging style throughout the interaction.And create one at a time after your answer the question and ask another one related to that.Also provide most common answer option like 3 or 4 of them and the last option is always give your own answer.",
+  systemInstruction: process.env.SYSTEM_INSTRUCT,
 });
 
 const generationConfig = {
