2cadf437c8faa2bd681da98431525cebe783cecd
sohan01fw <sohancwactfw@gmail.com>
2024-09-27 12:10:22 +0545

    adding instruction

--- src/lib/actions/ai-actions.ts
+++ src/lib/actions/ai-actions.ts
@@ -6,6 +6,7 @@ import { google } from "@ai-sdk/google";
 export async function continueConversation(messages: CoreMessage[]) {
   const result = await streamText({
     model: google("gemini-1.5-flash"),
+    system: process.env.SYSTEM_INSTRUCT,
     messages,
   });
 
